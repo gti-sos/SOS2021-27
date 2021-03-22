6f6d6a32dd137058c7f271e944698b0cb5f0dafe
Jota0Garcia <javier.garcia.herrera2000@gmail.com>
2021-03-22 12:06:58 +0100

    Configurado servidor y carpeta public

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var cool = require("cool-ascii-faces");
 var express=require("express");
 
 var app=express();
-var port=10000;
+var port= (process.env.PORT || 10000); // Obtiene el puerto que se le indique o el 10000 en caso de no existir
+
 app.get("/cool", (request,response)=>{ 
     response.send(cool());
     console.log("New Request to /cool has arrived");
@@ -12,3 +13,10 @@ app.get("/cool", (request,response)=>{
 app.listen(port, () =>{
     console.log("Server ready listening on port "+port);
 });
+
+
+var path=require("path");
+
+// Dirname obtiene la carpeta donde se ejecuto npm start (C:\\....). Path permite concatenar directorios con join, para tener un metodo estandar en windows y linux.
+// Use hace que cuando se llame a lo que sea que esté dentro de /, lo busque en la carpeta public
+app.use("/",express.static(path.join(__dirname + "\\public"))); 
