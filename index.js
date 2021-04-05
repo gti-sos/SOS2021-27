1cc225858a0ad78d383dbd3a9e597bd9c4ca1939
FerNueJim <nano_1326@hotmail.com>
2021-04-05 17:00:41 +0200

    api

--- index.js
+++ index.js
@@ -124,11 +124,11 @@ app.get("/info/province-budget-and-investment-in-social-promotion",(request,resp
 
 var budgets = [];
 
-app.get(BASE_API_PATH+"/budgets", (request, response) => {
+app.get(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion", (request, response) => {
     response.sendStatus(JSON.stringify(budgets,null,2));
 })
 
-app.get(BASE_API_PATH+"/budgets/loadInitialData", (request, response) => {
+app.get(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion/loadInitialData", (request, response) => {
     budgets_ini = [
         {
             "province": 'SEVILLE',
@@ -154,6 +154,61 @@ app.get(BASE_API_PATH+"/budgets/loadInitialData", (request, response) => {
     console.log(`Loaded Initial Data: <${JSON.stringify(budgets_ini, null, 2)}>`);
 	return response.sendStatus(200);
 })
+
+app.post(BASE_API_PATH+"/pC", (request,response)=>{ 
+	var data = request.body;
+	budgets.push(data)
+    console.log(`Resource stored: <${JSON.stringify(data, null, 2)}>`);
+    response.sendStatus(201)
+});
+
+app.get(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion/:province/:year", (request, response)=>{
+    var budget = budgets.filter(function(s){
+        return s.province==String(request.params.province)&& s.year==String(request.params.year);});
+
+        response.status(200).send(JSON.stringify(budget,null,2));
+   
+}) ;    
+
+app.delete(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion/:province/:year", function(request, response) { 
+	budgets = budgets.filter(function(s){
+		if(s.province!=String(request.params.province) || s.year!=(String(request.params.year))) {
+			return s;
+		}
+	});
+
+	response.status(200).send("Eliminated correctly");
+});
+
+app.put(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion/:province/:year", function(request, response) { 
+
+	for(var s in budgets){
+		
+		if(budgets[s].province == String(request.params.province) &&
+			budgets[s].year == String(request.params.year)){
+				var changes = req.body;
+				budgets[s] = changes;
+				break;
+		}
+	}
+    response.status(200).send("Updated correctly");
+
+});
+app.post(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion/:province/:year", function(request, response) { 
+
+	response.status(405).send("Method not allowed"); 
+});
+app.put(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion", function(request, response) { 
+
+	response.status(405).send("Method not allowed"); 
+});
+
+app.delete(BASE_API_PATH+"/province-budget-and-investment-in-social-promotion", (request,response)=>{
+		
+	budgets = []; 
+	response.status(200).send("Everything has been eliminated");
+
+});
 
 
 //API suicide-records
