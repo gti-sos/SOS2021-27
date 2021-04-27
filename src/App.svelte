7504667283833a2a465b20cf873fefbac194d89e
Humberto Garcia <humbertoinformatico@gmail.com>
2021-04-27 18:14:45 +0200

    mm

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,14 @@
 	import NotFound from './notFound.svelte';
 	import SuicideRecord from './suicideFrontEnd/suicide.svelte';
 	import Budgets from './budgetsFrontEnd/budget.svelte';
+import Activities from './activitiesFrontEnd/activities.svelte';
 
 
 	const routes={
 		'/':Home,
 		'/suicide-records':SuicideRecord,
 		'/province-budget-and-investment-in-social-promotion':Budgets,
-		'*':NotFound
+		'/azar-games-and-bet-activities':Activities
 	};
 
 </script>
