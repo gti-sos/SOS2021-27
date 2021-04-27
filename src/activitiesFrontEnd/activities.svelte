ae2191c68e42b3aaf8f47b4ba2c266baedaae0b5
Humberto Garcia <humbertoinformatico@gmail.com>
2021-04-27 18:07:24 +0200

    df

---
+++ src/activitiesFrontEnd/activities.svelte
@@ -0,0 +1,8 @@
+<script>
+    import ActivitiesTable from './ActivitiesTable.svelte'
+</script>
+
+<main>
+    <h1>Registro de actividad en loteria.</h1>
+    <ActivitiesTable></ActivitiesTable>
+</main>
