4d28fa88c0ecc6479f6a6259746b56f168940a9c
FerNueJim <nano_1326@hotmail.com>
2021-04-27 14:39:57 +0200

    svelte budgets

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,13 @@
 	import Home from './Home.svelte';
 	import NotFound from './notFound.svelte';
 	import SuicideRecord from './suicideFrontEnd/suicide.svelte';
+	import Budgets from './budgetsFrontEnd/budget.svelte';
 
 
 	const routes={
 		'/':Home,
 		'/suicide-records':SuicideRecord,
+		'/province-budget-and-investment-in-social-promotion': Budgets,
 		'*':NotFound
 	};
 
