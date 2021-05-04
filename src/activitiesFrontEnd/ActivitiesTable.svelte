c5ee19cf90c5919b7b77a8f4c6f354a487f86121
Humberto Garcia <humbertoinformatico@gmail.com>
2021-05-04 15:05:34 +0200

    fin

--- src/activitiesFrontEnd/ActivitiesTable.svelte
+++ src/activitiesFrontEnd/ActivitiesTable.svelte
@@ -1,6 +1,5 @@
 <script>
     import { onMount } from "svelte";
-
     import Table from "sveltestrap/src/Table.svelte";
     import Button from "sveltestrap/src/Button.svelte";
 
--- src/activitiesFrontEnd/activities.svelte
+++ src/activitiesFrontEnd/activities.svelte
@@ -6,3 +6,10 @@
     <h1>Registro de actividad en loteria.</h1>
     <ActivitiesTable></ActivitiesTable>
 </main>
+<style>
+    .titulo {
+        background-color: #FFB833;
+        color: #FFFFFF;
+        text-align: center;
+    }
+</style>
