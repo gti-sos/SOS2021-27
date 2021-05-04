9816a226435db7d6dc61023820a6623774f7f4d9
Humberto Garcia <humbertoinformatico@gmail.com>
2021-05-04 15:14:11 +0200

    nn

--- src/activitiesFrontEnd/activities.svelte
+++ src/activitiesFrontEnd/activities.svelte
@@ -6,10 +6,4 @@
     <h1>Registro de actividad en loteria.</h1>
     <ActivitiesTable></ActivitiesTable>
 </main>
-<style>
-    .titulo {
-        background-color: #FFB833;
-        color: #FFFFFF;
-        text-align: center;
-    }
-</style>
+
