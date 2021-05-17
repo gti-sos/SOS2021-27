3f5ca1ce2370624b3bff84375ba6c568b4c7a33b
FerNueJim <nano_1326@hotmail.com>
2021-05-18 00:44:47 +0200

    graph conjunto

--- src/frontEnd/Graphs.svelte
+++ src/frontEnd/Graphs.svelte
@@ -98,7 +98,7 @@
 
       Highcharts.chart("container", {
         title: {
-          text: "Registro de suicidios | Inversion promoción social | Actividad en loteria",
+          text: "",
         },
         yAxis: {
           title: {
@@ -165,13 +165,13 @@
     }
   </script>
   
-  <svelte:head>
+<svelte:head>
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/series-label.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js" on:load={loadGraph}></script>
-    </svelte:head>
+</svelte:head>
   
   <main>
   <body>
