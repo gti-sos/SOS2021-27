9415dc92a097f39597f885c26a9870bc4ec1ce2e
Jota0Garcia <javier.garcia.herrera2000@gmail.com>
2021-06-02 01:04:50 +0200

    bug3

--- src/frontEnd/budgetsAPI/budgetsGraphicSigma.svelte
+++ src/frontEnd/budgetsAPI/budgetsGraphicSigma.svelte
@@ -1,24 +1,16 @@
 <script>
 
-
   
-
-
-
-
-
     import { onMount } from "svelte";
     import {Jumbotron, Navbar, Nav, NavItem, NavLink, NavbarBrand, Dropdown, DropdownToggle, DropdownMenu, DropdownItem,} from 'sveltestrap';
+
+    
+
 
     let isOpen = false;
 
 
-
-    /* Te lo comento pa q ya lo arregles cuando puedas q sino no me deja hacer na en toda la pagina <-----------
-
-
-
-
+    /*
     var BASE_API_PATH = "/api/v2/province-budget-and-investment-in-social-promotion";
 
     let budgetGraph = [];
@@ -89,75 +81,96 @@
     }
   ]
   });
-  */ 
+  */
 </script>
 
+
+
+
 <svelte:head>
-  <script src="sigma.min.js" on:load={loadGraph}></script>
+  
+  
   <script src="sigma.parsers.json.min.js"></script>
 </svelte:head>
 
 <main>
-    <body>
+  <body>
     <Jumbotron class="p-3" style="background-color: #FFB833">
-        <h1 class="titulo; mainDiv" style="color: white">Presupuesto por provincia y año, e inversión en promoción social</h1>
+      <h1 class="titulo; mainDiv" style="color: white">
+        Presupuesto por provincia y año, e inversión en promoción social
+      </h1>
     </Jumbotron>
-        <Navbar style="background-color: #FFB833; color:white;" light expand="lg" >
-            <NavbarBrand href="#/">INICIO</NavbarBrand>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="#/suicide-records">Registro de suicidios</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#/province-budget-and-investment-in-social-promotion">Presupuesto/Inversión</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#/azar-games-and-bet-activities">Actividad en loteria</NavLink>
-              </NavItem>
-              <Dropdown nav {isOpen} toggle="{() => isOpen = !isOpen}">
-                <DropdownToggle nav caret> Gráficas </DropdownToggle>
-                <DropdownMenu end>
-                  <DropdownItem href="#/graphics/suicide-records">Registro de suicidios</DropdownItem>
-                  <DropdownItem href="#/graphics/province-budget-and-investment-in-social-promotion">Presupuesto/Inversión</DropdownItem>
-                  <DropdownItem href="#/graphics/azar-games-and-bet-activities">Actividad en loteria</DropdownItem>
-                  <DropdownItem divider/>
-                  <DropdownItem href="#/graphics">Conjunto</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </Nav>
-        </Navbar>
-    </body>
-    <br>
-    <h1 class="titulo2"> Gráfica de datos </h1>
-    <div id="container">
-          <p class="centrado"> Gráfica que relaciona el presupuesto de cada provincia y año con la inversión que realiza cada una de estas en promoción social de tipo Sigma. </p>
-      </div>
+    <Navbar style="background-color: #FFB833; color:white;" light expand="lg">
+      <NavbarBrand href="#/">INICIO</NavbarBrand>
+      <Nav navbar>
+        <NavItem>
+          <NavLink href="#/suicide-records">Registro de suicidios</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#/province-budget-and-investment-in-social-promotion"
+            >Presupuesto/Inversión</NavLink
+          >
+        </NavItem>
+        <NavItem>
+          <NavLink href="#/azar-games-and-bet-activities"
+            >Actividad en loteria</NavLink
+          >
+        </NavItem>
+        <Dropdown nav {isOpen} toggle={() => (isOpen = !isOpen)}>
+          <DropdownToggle nav caret>Gráficas</DropdownToggle>
+          <DropdownMenu end>
+            <DropdownItem href="#/graphics/suicide-records"
+              >Registro de suicidios</DropdownItem
+            >
+            <DropdownItem
+              href="#/graphics/province-budget-and-investment-in-social-promotion"
+              >Presupuesto/Inversión</DropdownItem
+            >
+            <DropdownItem href="#/graphics/azar-games-and-bet-activities"
+              >Actividad en loteria</DropdownItem
+            >
+            <DropdownItem divider />
+            <DropdownItem href="#/graphics">Conjunto</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </Nav>
+    </Navbar>
+  </body>
+  <br />
+  <h1 class="titulo2">Gráfica de datos</h1>
+  <div id="container">
+    <p class="centrado">
+      Gráfica que relaciona el presupuesto de cada provincia y año con la
+      inversión que realiza cada una de estas en promoción social de tipo Sigma.
+    </p>
+  </div>
 </main>
 
 <style>
-    .titulo {
-        background-color: #FFB833;
-        color: #FFFFFF;
-        text-align: center;
-    }
-    
-    .titulo2 {
-        color: #000000;
-        text-align: center;
-        font-size: 150%;
-    }
-    .mainDiv{
-        text-align: center;
-        margin: 20px;
-    }
-    .centrado {
-        text-align: center;
-        padding: 1em;
-        margin: 0;
-    }
-    #container {
-      max-width: 400px;
-      height: 400px;
-      margin: auto;
-    }
+  .titulo {
+    background-color: #ffb833;
+    color: #ffffff;
+    text-align: center;
+  }
+
+  .titulo2 {
+    color: #000000;
+    text-align: center;
+    font-size: 150%;
+  }
+  .mainDiv {
+    text-align: center;
+    margin: 20px;
+  }
+  .centrado {
+    text-align: center;
+    padding: 1em;
+    margin: 0;
+  }
+  #container {
+    max-width: 400px;
+    height: 400px;
+    margin: auto;
+  }
 </style>
+
