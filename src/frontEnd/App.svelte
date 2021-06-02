b6ec4384fb35f0e0f7ddc2a3ba8330eff9218d29
Humberto Garcia <humbertoinformatico@gmail.com>
2021-06-02 19:42:43 +0200

    dd

--- src/frontEnd/App.svelte
+++ src/frontEnd/App.svelte
@@ -16,8 +16,7 @@
 	import SuicideGraphUvChart from './suicidesAPI/suicideGraphUvChart.svelte';
 	import suiCovid from './suicidesAPI/covidGraph.svelte';
 
-	import ActivitiesGraph2 from './activitiesAPI/activitiesGraph2.svelte';
-	import ActivitiesGraphVis from './activitiesAPI/activitiesGraphVis.svelte';
+
 	import Integrations from './Integrations.svelte'
 
 
