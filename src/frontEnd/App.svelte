366f842368e5805573f12c577a339acd3cd138af
FerNueJim <nano_1326@hotmail.com>
2021-06-03 18:05:28 +0200

    .,

--- src/frontEnd/App.svelte
+++ src/frontEnd/App.svelte
@@ -4,9 +4,9 @@
 	import SuicideRecord from './suicidesAPI/suicide.svelte';
 	import SuicideEdit from './suicidesAPI/editSuicide.svelte';
 	import SuicideGraph from './suicidesAPI/suicideGraph.svelte';
-	//import Budgets from './budgetsAPI/budget.svelte';
-	//import BudgetsEdit from './budgetsAPI/editBudget.svelte';
-	//import BudgetsGraphics from './budgetsAPI/BudgetsGraphics.svelte';
+	import Budgets from './budgetsAPI/budget.svelte';
+	import BudgetsEdit from './budgetsAPI/editBudget.svelte';
+	import BudgetsGraphics from './budgetsAPI/BudgetsGraphics.svelte';
 	import Activities from './activitiesAPI/activities.svelte';
 	import ActivitiesEdit from './activitiesAPI/editactivities.svelte';
 	import ActivitiesGraph from './activitiesAPI/activitiesGraph.svelte';
@@ -35,9 +35,9 @@
 		'/graphics/suicide-records3':SuicideGraphUvChart,
 		'/graphics/integrations/covid':suiCovid,
 		'/graphics/integrations/dogs':suiDogs,
-		//'/province-budget-and-investment-in-social-promotion':Budgets,
-		//'/province-budget-and-investment-in-social-promotion/:province/:year':BudgetsEdit,
-		//'/graphics/province-budget-and-investment-in-social-promotion':BudgetsGraphics,
+		'/province-budget-and-investment-in-social-promotion':Budgets,
+		'/province-budget-and-investment-in-social-promotion/:province/:year':BudgetsEdit,
+		'/graphics/province-budget-and-investment-in-social-promotion':BudgetsGraphics,
 		'/azar-games-and-bet-activities':Activities,
 		'/azar-games-and-bet-activities/:province/:year':ActivitiesEdit,
 		'/graphics/azar-games-and-bet-activities':ActivitiesGraph,
