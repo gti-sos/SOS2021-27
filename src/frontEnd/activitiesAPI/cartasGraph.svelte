a1228af4df7c54eb72da1881466ffab8b6c3ea4f
Humberto Garcia <humbertoinformatico@gmail.com>
2021-06-04 20:50:58 +0200

    dsf

--- src/frontEnd/activitiesAPI/cartasGraph.svelte
+++ src/frontEnd/activitiesAPI/cartasGraph.svelte
@@ -1,82 +1,92 @@
 <script>
     import Button from "sveltestrap/src/Button.svelte";
     import { pop } from "svelte-spa-router";
-    let extData = [];
-    let countryName = [];
-    let countryPopulation = [];
-    async function loadChart() {
-        const res = await fetch("https://restcountries.eu/rest/v2/lang/es");
-        extData = await res.json();
-        if (res.ok) {
-          extData.forEach((stat) => {
-            countryName.push(stat.name);
-            countryPopulation.push(stat.population);
-          });
-        }
     
-    console.log(extData);
-        Highcharts.chart("container", {
-            chart: {
-        type: 'lollipop'
-    },
-    legend: {
-        enabled: false
+async function loadChart(){
+   
+    var cards={};
+    var cardTypes = [];
+    const resData = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php");
+    const cardData = await resData.json();  
+    console.log(cardData);
+    
+    cardData.data.forEach((v) =>{
+         if(v.type in cards){
+                cards[v.type] += 1;
+            }
+            else{
+                cards[v.type]= 1;
+            }
+        });
+    
+    for (var key in cards){
+        cardTypes.push([key,cards[key]]);
+    }
+    
+    Highcharts.chart('container', {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
     },
     title: {
-        text: 'Población de países de habla hispana'
+        text: 'Cartas de Yu-Gi-Oh según su tipo'
     },
-    tooltip: {
-        shared: true
-    },
-    xAxis: {
-        type: 'category',
-        categories: countryName
-    },
-    yAxis: {
-        title: {
-            text: 'Población'
+    accessibility: {
+        point: {
+            valueSuffix: '%'
         }
     },
-          series: [
-            {
-              name: "Población",
-              data: countryPopulation,
-              colorByPoint: true
-            },
-          ],
-        });
-      }
-    </script>
-    
-    <svelte:head>
-<script src="https://code.highcharts.com/highcharts.js"on:load={loadChart}></script>
-<script src="https://code.highcharts.com/highcharts-more.js"></script>
-<script src="https://code.highcharts.com/modules/dumbbell.js"></script>
-<script src="https://code.highcharts.com/modules/lollipop.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script>
-    </svelte:head>
-    
-    <main>
-        <div>
-            <h1 style="text-align: center;"><strong>Uso API Ext. Restcountries</strong></h1>
-          </div>
-        
-        <div>
-            <figure class="highcharts-figure">
-              <div id="container" />
-              <p style="text-align: center;" class="highcharts-description">
-                Gráfico lollipop que muestra la cantidad de habitantes en los países de habla hispana.
-              </p>
-            </figure>
-            <Button id="back" outline color="secondary" on:click="{pop}"> Atrás</Button>
-          </div>
-    </main>
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
+    },
+    series: [{
+        name: 'Porcentaje de cartas',
+        data: cardTypes
+    }]
+});
+}
+</script>
+<svelte:head>
+  <script src="https://code.highcharts.com/highcharts.js" on:load={loadChart}></script>
+  <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+  <script src="https://code.highcharts.com/modules/exporting.js"></script>
+  <script src="https://code.highcharts.com/modules/export-data.js"></script>
+  <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+</svelte:head>
+
+<main>
+  <figure class="highcharts-figure">
+  <div id ="container"></div>
+  <p class="highcharts-description">
+      Gráfico 3D que muestra la cantidad de cartas de YU-GI-OH segun su tipo.
+  </p>
+  </figure>
+    <Button id="back" outline color="secondary" on:click="{pop}"> Atrás</Button>
+</main>
 
 <style>
-.highcharts-figure{
-    min-width: 320px; 
-    max-width: 970px;
+#container {
+    height: 600px;
+    width: 900px;
+}
+.highcharts-figure {
+    min-width: 350px;
+    max-width: 900px;
     margin: 1em auto;
 }
-    </style>
+</style>
