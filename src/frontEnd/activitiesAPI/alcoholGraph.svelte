ad627cffccec95babdaa03ce15d2076da4861411
Humberto Garcia <humbertoinformatico@gmail.com>
2021-06-04 19:18:47 +0200

    sdf

--- src/frontEnd/activitiesAPI/alcoholGraph.svelte
+++ src/frontEnd/activitiesAPI/alcoholGraph.svelte
@@ -1,4 +1,13 @@
 <script>
+
+
+import {
+        onMount
+    } from "svelte";
+
+    import {Jumbotron, Navbar, Nav, NavItem, NavLink, NavbarBrand, Dropdown, DropdownToggle, DropdownMenu, DropdownItem,} from 'sveltestrap';
+    let isOpen = false;
+
     async function loadGraph(){
     let covid = []
     let covidConfirmado = []
@@ -125,6 +134,54 @@
 
 
 <main>
-    <h1> Grafica de la API Awards </h1>
-    <div id="myChart"></div>
-</main>
+    <body>
+        <Jumbotron class="p-3" style="background-color: #FFB833">
+            <h1 class="titulo; mainDiv" style="color: white">Actividad en juegos de azar por  provincia y año</h1>
+        </Jumbotron>
+            <Navbar style="background-color: #FFB833; color:white;" light expand="lg" >
+                <NavbarBrand href="#/">INICIO</NavbarBrand>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink href="#/suicide-records">Registro de suicidios</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="#/province-budget-and-investment-in-social-promotion">Presupuesto/Inversión</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="#/azar-games-and-bet-activities">Actividad en loteria</NavLink>
+                  </NavItem>
+                  <Dropdown nav {isOpen} toggle="{() => isOpen = !isOpen}">
+                    <DropdownToggle nav caret> Gráficas </DropdownToggle>
+                    <DropdownMenu end>
+                      <DropdownItem href="#/graphics/suicide-records">Registro de suicidios</DropdownItem>
+                      <DropdownItem href="#/graphics/province-budget-and-investment-in-social-promotion">Presupuesto/Inversión</DropdownItem>
+                      <DropdownItem href="#/graphics/azar-games-and-bet-activities">Actividad en loteria</DropdownItem>
+                      <DropdownItem divider/>
+                      <DropdownItem href="#/graphics">Conjunto</DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </Nav>
+            </Navbar>
+        </body>
+        <br>
+        <h1 class="titulo2"> Gráfica de datos </h1>
+       
+          <div id="myChart"></div>
+      
+      
+      </main>
+      
+      <style>
+      
+        
+        .titulo2 {
+            color: #000000;
+            text-align: center;
+            font-size: 150%;
+        }
+        .mainDiv{
+            text-align: center;
+            margin: 20px;
+        }
+      
+      </style>
