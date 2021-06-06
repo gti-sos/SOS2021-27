02fd0adde192a72f1979c8fa0914d1d1be06c45f
Humberto Garcia <humbertoinformatico@gmail.com>
2021-06-06 16:46:48 +0200

    sdf

--- src/frontEnd/activitiesAPI/povertyriskGraph.svelte
+++ src/frontEnd/activitiesAPI/povertyriskGraph.svelte
@@ -1,110 +1,155 @@
 <script>
-    import { pop }from "svelte-spa-router";
-    import { Button } from "sveltestrap";
-    import Dygraph from 'dygraphs';
-    //Uso API grupo 11
-    const BASE_API_PATH = "/api/v2";
-    var povertyStats = [];
-    var alcoholData = [];
-    var errorMsg = "";
-    console.log("Cargando página...");
-    async function getStats() {
-        console.log("Fetching alcohol data...");
-        const res = await fetch(BASE_API_PATH + "/alcohol-consumption-stats/loadInitialData");
-        if (res.ok) {
-          console.log("OK");
-          alcoholData = await res.json();
-          console.log("Datos alcohol recibidos ahora mismo")
-          console.log(alcoholData);
-          console.log(`We have received ${alcoholData.length} alcohol-consumption-stats.`);
-        } else {
-          console.log("Error");
-          errorMsg = "Error al cargar los datos de la API";
-        }
-    }
-    async function getPovertyStats() {
-        console.log("Fetching poverty data...");
-        await fetch("https://endpoint-poverty-risks.herokuapp.com/api/v1/loadInitialData");
-        const res = await fetch("https://endpoint-poverty-risks.herokuapp.com/api/v1/");
-        console.log(res);
-        if (res.ok) {
-          const json = await res.json();
-          povertyStats = json;
-          console.log(`We have received ${povertyStats.length} poverty-stats.`);
-          console.log(povertyStats);
-          console.log("Ok");
-        } else {
-          errorMsg = "Error recuperando datos de pobreza";
-          console.log("ERROR!" + errorMsg);
-        }
-    }
+    import Button from "sveltestrap/src/Button.svelte";
+    import {
+        pop
+    } from "svelte-spa-router";
+   var CBData= [];
+    var yAxe=[];
     async function loadGraph() {
-      console.log("Inicio getStats loadGraph");
-      await getPovertyStats();
-      await getStats();
-      console.log('Datos alcohol recibidos para pintar el grafo:');
-      console.log(alcoholData);
-      console.log('Datos pobreza recibidos para pintar el grafo:');
-      console.log(povertyStats);
-      let mediaPobreza = [];
-      for (let index = 0; index < povertyStats.length; index++) {
-        mediaPobreza.push(povertyStats[index].people_in_risk_of_poverty);
-      }// Calcular media y representarlo en una linea en el grafico , ampliar los campos del grafico a 4
-      let arrayDatos = [];
-      for (let index = 0; index < alcoholData.length; index++) {
-        let separa = alcoholData[index].ageRange.split('-'); 
-        let parseo = parseInt(separa[1]);
-        arrayDatos.push([parseo,alcoholData[index].alcoholPrematureDeath,povertyStats[index].percentage_risk_of_poverty]);
+            
+        /* Asking for the data to the back */
+        const BASE_API_URL = "https://sos2021-07.herokuapp.com/api/v2/unemployment";
+        const res_CB_Data = await fetch("https://sos2021-27.herokuapp.com/api/v2/azar-games-and-bet-activities/"
+);
+        CBData = await res_CB_Data.json();
+        for (var i=0; i<CBData.length; i++){
+            yAxe.push(CBData[i].state);
+        }
+        const resData = await fetch(BASE_API_URL);
+        let MyData = await resData.json();   
+        /* Getting the countries */
+        let countries = Array.from(new Set(MyData.map((d) => {return d.autonomous_community;})));
+       
+        MyData = MyData.map((d) => {
+            return [countries.indexOf(d.autonomous_community),d["youth_unemployment_rate"], d["unemployment_rate"], d["occupation_variation"]]; 
+        });
+        /* 
+        The following array turn this:
+        [0, 2000, 0.0, 0.0, 0.0]
+        into this:
+        {name: "Country", data: [0, 2000, 0.0, 0.0, 0.0]}
+        That is how the chart needs it
+         */
+        let ChartData = MyData.map(function (set, i) {
+            for(var i=0; i<CBData.length; i++){
+            return {
+                name: "Comunidades y estados",
+                data: set,
+                shadow: false
+            }
+        };
+    });
         
-      } // Etiqueta (Valorx) Numero asociado al rango de edad, Dato grafica muertes , Dato grafica ansiedad
-      console.log("Array de datos para el grafo:");
-      console.log(arrayDatos);
-      new Dygraph(document.getElementById("grafo1"),arrayDatos,
-      { 
-              labels:["RangoEdad","Muertes","Porcentaje riesgo de pobreza mundial"],
-              legend: 'always',
-              title: 'Muertes prematuras y ansiedad España 2017',
-              titleHeight: 32,
-              ylabel: 'Valor',
-              xlabel: 'Rango de edad'
+        /* Setting the chart */
+        Highcharts.chart('container', {
+            chart: {
+                type: 'spline',
+                parallelCoordinates: true,
+                parallelAxes: {
+                    lineWidth: 3
+                }
+            },
+            title: {
+                text: 'Energías renovables'
+            },
+            plotOptions: {
+                series: {
+                    animation: false,
+                    marker: {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false
+                            }
+                        }
+                    },
+                    states: {
+                        hover: {
+                            halo: {
+                                size: 0
+                            }
+                        }
+                    },
+                    events: {
+                        mouseOver: function () {
+                            this.group.toFront();
+                        }
+                    }
+                }
+            },
+            tooltip: {
+                pointFormat: '<span style="color:{point.color}">\u25CF</span>' +
+                    '{series.name}: <b>{point.formattedValue}</b><br/>'
+            },
+            xAxis: {
+                categories: [
+                    'Estados',
+                    'Variación de ocupación',
+                    'Ratio de desempleo joven',
+                    'Ratio de desempleo',
+                    ''
+                ],
+                offset: 50
+            },
+            yAxis: [
+            {
+                categories: yAxe[2] ,
+                tooltipValueFormat: '{value}'
+            },
+            {
+                min: 0,
+                tooltipValueFormat: '{value}'
+            }, {
+                min: 0,
+                tooltipValueFormat: '{value} %'
+            }, {
+                min: 0,
+                tooltipValueFormat: '{value} %'
+            }],
+            colors: ['rgba(129, 131, 202, 0.8)'],
+            series: ChartData
+        });
+    }
+    loadGraph();
+</script>
+
+<svelte:head>
+    <script src="https://code.highcharts.com/highcharts.js"  defer> </script>
+    <script src="https://code.highcharts.com/modules/data.js"  defer ></script>
+    <script src="https://code.highcharts.com/modules/parallel-coordinates.js"  defer></script>
+	<script src="https://code.highcharts.com/modules/exporting.js"  defer ></script>
+	
+	<!--Highcharts Oil Coal...-->
+	<script src="https://code.highcharts.com/highcharts.js" defer  ></script>
+    <script src="https://code.highcharts.com/modules/heatmap.js" defer ></script>
+    <script src="https://code.highcharts.com/modules/exporting.js" defer  ></script>
+    <script src="https://code.highcharts.com/modules/export-data.js" defer ></script>
+	<script src="https://code.highcharts.com/modules/accessibility.js" defer ></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" integrity="sha256-R4pqcOYV8lt7snxMQO/HSbVCFRPMdrhAFMH+vr9giYI=" crossorigin="anonymous"></script>
+	
+    <script src="https://code.highcharts.com/highcharts.js"  defer> </script>
+    <script src="https://code.highcharts.com/modules/data.js"  defer ></script>
+    <script src="https://code.highcharts.com/modules/parallel-coordinates.js" defer></script>
+    <script src="https://code.highcharts.com/modules/exporting.js" defer ></script>
+
+	<!--Highcharts Oil Coal...-->
+    <script src="https://code.highcharts.com/modules/heatmap.js"  defer></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"  defer></script>
+	<script src="https://code.highcharts.com/modules/accessibility.js"  defer></script>
     
-            });
-    };
-  </script>
-  
-  <svelte:head>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/dygraph/2.1.0/dygraph.min.js" on:load={loadGraph}></script>
-  </svelte:head>
-  
-  <main>
-    <div>
-      <h2>Integración API SOS poverty-stats</h2>
-    </div>
-    {#if errorMsg}
-      <p>{errorMsg}</p>
-    {:else}
-    <style>.dygraph-legend { text-align: right; background: none; }
-        #grafo1 .dygraph-label { font-family: Georgia, Verdana, serif; }
-        #grafo1 .dygraph-title { font-size: 20px; text-shadow: gray 2px 2px 2px; margin: -30px 0px 0px 50px}
-        #grafo1 .dygraph-ylabel { font-size: 18px; text-shadow: gray -2px 2px 2px; margin: 0px 0px 0px 90px }
-        #grafo1 .dygraph-xlabel { font-size: 18px; text-shadow: gray -2px 2px 2px; margin: 20px 0px 0px 0px }
-        .chart { border: 1px hidden black; margin: 50px 5px 5px 400px; padding: 2px; }
-    </style>
-    <div  id="grafo1" class="chart" style="width:600px; height:300px;"></div>
-    <br>
-    <br>
-    <h6>Gráfico en dónde se muestra el riesgo de pobreza junto con las muertes prematuras</h6>
-    {/if}
-    <Button outline color="secondary" on:click="{pop}">Atrás</Button>
-  </main>
-  
-  <style>
-    main {
-      text-align: center;
-      padding: 1em;
-      margin: 0 auto;
-    }
-    div {
-      margin-bottom: 15px;
-    }
-  </style>
+</svelte:head>
+
+<main>
+    <h1 class="display-4" style="text-align: center;" > Gráfica con HighChart </h1>
+    <figure class="highcharts-figure">
+        <div id="container"></div>
+        <p></p>
+        <p class="highcharts-description">
+            Gráfica que representa los datos de las energías renovables por países. Los datos de las energías renovables son los porcentajes de: uso de energías renovables sobre el total, uso de energía hidroeléctrica y uso de energía eólica.
+            Esta gráfica está hecha con HighChart.
+        </p>
+    </figure>
+
+    <p></p>
+    <Button outline color="secondary" on:click="{pop}"> <i class="fas fa-arrow-circle-left"></i> Atrás </Button>
+</main>
