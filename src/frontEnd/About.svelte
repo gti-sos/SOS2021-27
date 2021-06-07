f3be2d68a5e4c663e941905e3132febb3a0ddb79
Jota0Garcia <javier.garcia.herrera2000@gmail.com>
2021-06-07 23:24:32 +0200

    video fernando

--- src/frontEnd/About.svelte
+++ src/frontEnd/About.svelte
@@ -71,6 +71,7 @@
       <slot>
         <div>
           <h3 class="titulo"><b>Vídeo de Fernando Nuevo</b></h3>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/BFnAANVKiBo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            <br>
            <div class="titulo">
           
